bc4930276602b3f0f3b84d0384ce6f44296d170e
Leenclair <leesu9317@gmail.com>
2022-07-16 18:46:25 +0900

    3강까지 정리

---
+++ src/main/java/com/leen/aws/springboot/web/dto/PostsResponseDto.java
@@ -0,0 +1,21 @@
+package com.leen.aws.springboot.web.dto;
+
+import com.leen.aws.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity){
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+
+}
